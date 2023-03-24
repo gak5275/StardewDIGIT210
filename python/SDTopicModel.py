bbc0e5e966787108d9d3052d7f9313ba949c523b
William Stiller <wxs5242@psu.edu>
2023-03-24 13:16:24 -0400

    cleaned topic modeling code

--- python/SDTopicModel.py
+++ python/SDTopicModel.py
@@ -18,21 +18,23 @@ from saxonche import PySaxonProcessor
 import os
 
 
+# Stopwords
 stop_words = stopwords.words('english')
 
-newStopWords = ["said", "one", "go", "went", "came", "like", "one"]
+newStopWords = []
 stop_words.extend(newStopWords)
 
 workingDir = os.getcwd()
-print('THIS IS WORKING DIRECTORY', workingDir)
 parentDir = workingDir.removesuffix('python')
 stardewPath = os.path.join(parentDir, 'xmlfiles')
-print('THIS IS THE FILEPATH FOR ALL FILES', stardewPath)
+print('WORKING DIRECTORY:', workingDir)
+print('FILEPATH:', stardewPath)
 
-# cleaning documents:
+
+# Clean the Documents:
 def clean_doc(doc):
     print("RUNNING CLEANDOC")
-    print("THIS IS THE DOC: ", doc)
+    print(doc)
     with PySaxonProcessor(license=False) as proc:
         xml = open(doc, encoding='utf-8').read()
 
@@ -42,14 +44,10 @@ def clean_doc(doc):
 
         text = xp.evaluate('//dialogue ! text()')
         dialogue = text.__str__()
-    #print("THIS IS THE DIALOGUE", dialogue)
-    print("Class of dialogue", type(dialogue))
     no_punct = ''
     for c in dialogue:
         if c not in string.punctuation:
             no_punct = no_punct + c
-    # with list comprehension
-    # no_punct = ''.join([c for c in doc if c not in string.punctuation])
 
     words = no_punct.lower().split()
 
@@ -60,9 +58,9 @@ def clean_doc(doc):
 
     return final_words
 
-# ebb: This controls our file handling as a for loop over the directory:
+
+# Finding the Files
 allDocs = []
-#print('THIS IS ALL DOCS', allDocs)
 print('THIS IS OS.LISTDIR LIST:', os.listdir(stardewPath))
 
 fileList = os.listdir(stardewPath)
@@ -77,60 +75,26 @@ for file in fileList:
         print('This is a Folder:', f)
         newFiles = [file + '\\' + x for x in os.listdir(f)]
         fileList.extend(newFiles)
-#print(allDocs)
 
-# PREPARING THE CORPUS FOR TOPIC MODELING ########################
+
+# Preparing Corpus
 cleaned_docs = [clean_doc(doc) for doc in allDocs]
-# Add bigrams and trigrams to docs (only ones that appear 20 times or more).
-#print(cleaned_docs)
 bigram = Phrases(cleaned_docs, min_count=20)
 for idx in range(len(cleaned_docs)):
     for token in bigram[cleaned_docs[idx]]:
         if '_' in token:
-            # Token is a bigram, add to document.
             cleaned_docs[idx].append(token)
 
-#print(cleaned_docs)
-
-# ebb: We used this code to help us locate buggy text files. It will stop on files that can't be processed due to weird non-Unicode characters.
-# cleaned_docs = []
-# for doc in allDocs:
-#     print("This doc is going to the cleaners: " + f"{doc=}")
-#     clean_doc(doc)
-
 id2word = corpora.Dictionary(cleaned_docs)
-
-# print(id2word[260])
 corpus = [id2word.doc2bow(cleaned_doc) for cleaned_doc in cleaned_docs]
-#print("THIS IS THE CORPUS", corpus)
-
 
 lda_model = LdaModel(corpus=corpus, id2word=id2word, num_topics=30)
-# Suggestion: Try 10 - 50 topics and vary in 5s
 topics = lda_model.get_document_topics(corpus)
 print("Number of topics:", len(topics))
 sorted_topics = sorted(topics[1], key=lambda x: x[1], reverse=True)
 
-# ebb: So, let's see what's in a topic:
-for topic in topics[1][:10]:
-    #print(f"{topic=}")
-    # if topic[1] > .5:
-    # This asks for up to 10 topics in document 209. It'll be fine if 10 topic clusters aren't really available there.
-    terms = lda_model.get_topic_terms(topic[0], 15)
-    # topic[0] is not the same as topics. (topics are documents). topic is an actual topic.
-    # topic[0] is probably the heaviest weighted "top" topic.
-    for num in terms:
-        num = num[0]
-        #print(num, id2word[num])
-    #print()
 
-# ###### VISUALIZING THE TOPIC MODELS ####################
-# ebb: We're using the pyLDAvis (python LDA topic modeling vis) library to output an HTML file
-# that shows an interactive visualization. It will output an HTML file in your working directory.
-# You want to go and open that file in a web browser to view the model and adjust it.
-# HOW TO READ THE LDA Visualization:
-# See https://we1s.ucsb.edu/research/we1s-tools-and-software/topic-model-observatory/tmo-guide/tmo-guide-pyldavis/
-# As you inspect the visualization, you should come back to this script and
-# experiment with adding stop words and adjusting the number of topics to model.
+# Create the Visualization
 vis = pyLDAvis.gensim_models.prepare(lda_model, corpus, id2word, mds="mmds", R=30)
 pyLDAvis.save_html(vis, 'topicModel_Visualization.html')
+print("Visual Outputted")
